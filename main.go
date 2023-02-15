52f24d36c629f6f1d37a5146555104a85ee4703c
Longs Pemun Gotar <developerlongs@gmail.com>
2023-02-15 07:04:56 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	ctx := context.Background()
+
+	ethClient, err := ethclient.Dial("https://mainnet.infura.io/v3/ac7fe75399a146fe821d83ce4c7c512e")
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	for {
+		// Poll for new pending transactions
+		pendingTxs, err := ethClient.PendingTransactionCount(ctx)
+		if err != nil {
+			log.Printf("Error while getting pending transactions: %v", err)
+			break
+		} else {
+			fmt.Println("-------------------------------------------------------")
+			fmt.Printf("Pending transaction: %v\n", pendingTxs)
+		}
+		// Sleep for a few seconds before checking for new pending transactions
+		time.Sleep(time.Second * 5)
+	}
+}
